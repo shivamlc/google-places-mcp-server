f6539a36465a5e3a973f951704e3c0481a7b9094
shivamacshack <s.gaur@uq.edu.au>
2026-03-29 11:51:57 +1100

    Initial commit

---
+++ src/main/java/com/sgaur_tech/google_places_mcp_server/GooglePlacesMcpServerApplication.java
@@ -0,0 +1,13 @@
+package com.sgaur_tech.google_places_mcp_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglePlacesMcpServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooglePlacesMcpServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgaur_tech/google_places_mcp_server/GooglePlacesMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgaur_tech.google_places_mcp_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooglePlacesMcpServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
